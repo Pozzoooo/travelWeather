b1de6c6ef9f1bc8dc058788bbb0e649bf2186997
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2018-04-11 21:35:38 +0100

    reviewing todo list

--- app/src/main/java/pozzo/apps/travelweather/App.kt
+++ app/src/main/java/pozzo/apps/travelweather/App.kt
@@ -5,36 +5,15 @@ import com.splunk.mint.Mint
 
 /**
  * TODO
- * 1x
  * Fade in ao mostrar os marks, eh possivel?
- * Clarificar de alguma forma as acoes do usuario, tipo arrastar o marcador talvez.
- * Busca na action bar, e a selecao de dia (hoje, amananha...) pode ir em uma spinner no estilo
- * navegacao.
- * Quando houver um erro na criacao da rota (no estilo reportado do celular do pai) fazer um clear
- * para garantir que nao eh a posicao inicial o problema.
- * A distancia entre previsao deve ser dinamica, em uma distancia maior eu nao precio de tantas
- * previsoes.
- * Why sometimes the route button shows and some times it does not?
- * Initial tutorial?
+ * Busca na action bar, e a selecao de dia (hoje, amananha...) pode ir em uma spinner no estilo navegacao.
+ * A distancia entre previsao deve ser dinamica, em uma distancia maior eu nao precio de tantas previsoes.
  * Remove http legacy
- * Refactor this shity code
- * Try to implement mvvm
- * remove support library
- *
- * ?
  * Realizar a separacao early, late...
  * A questao do horario de saida e chegada, como fica?
- *
- * 2x
- * Persistir dados
  * Agendar uma viagem
- * Exibir ultima rota que estava sendo visualizada atraves do banco
  * Notificar quando o tempo mudar apos ter agendado uma viagem
  * Outra fonte para busca de previsao do tempo
- * Menu a cada quantos km realizar a previsao
- * Utilizar ExecutorService para requisicao de previsao
- *
- * Created by sarge on 10/19/15.
  */
 class App : Application() {
 
