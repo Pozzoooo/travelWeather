010355c21a0e7cb903ad91066dbb3896095dbf04
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2018-06-24 15:44:38 +0100

    Bettern handling on linked notification

--- app/src/main/java/pozzo/apps/travelweather/firebase/MessagingService.kt
+++ app/src/main/java/pozzo/apps/travelweather/firebase/MessagingService.kt
@@ -2,10 +2,15 @@ package pozzo.apps.travelweather.firebase
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.splunk.mint.Mint
 import pozzo.apps.travelweather.common.notification.NotificationHandler
 import pozzo.apps.travelweather.common.notification.NotificationVo
 
+//todo how much should I split this class?
 class MessagingService : FirebaseMessagingService() {
+    companion object {
+        private const val URL = "url"
+    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if (remoteMessage.data.isNotEmpty()) {
@@ -16,8 +21,21 @@ class MessagingService : FirebaseMessagingService() {
     private fun handleMessageWithData(remoteMessage: RemoteMessage) {
         val data = remoteMessage.data
 
-        //todo handle nullable parameters in a more reliable way
-        val notification = NotificationVo(data["url"]!!, data["customMessage"]!!)
-        NotificationHandler().linkedNotification(this, notification)
+        if (data.containsKey(URL)) {
+            handleLinkedNotification(remoteMessage)
+        }
+    }
+
+    private fun handleLinkedNotification(remoteMessage: RemoteMessage) {
+        val data = remoteMessage.data
+        val url = data[URL]
+        val customMessage = data["customMessage"]
+
+        if (url != null && customMessage != null) {
+            val notification = NotificationVo(url, customMessage)
+            NotificationHandler().linkedNotification(this, notification)
+        } else {
+            Mint.logException(Exception("Missing pieces: $url and $customMessage"))
+        }
     }
 }
