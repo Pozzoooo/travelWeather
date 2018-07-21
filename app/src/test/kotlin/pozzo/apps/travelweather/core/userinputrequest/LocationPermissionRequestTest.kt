504453666923843d273d238daf376a8d26852b22
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2018-07-21 23:22:17 +0100

    LocationPermissionRequestTest

---
+++ app/src/test/kotlin/pozzo/apps/travelweather/core/userinputrequest/LocationPermissionRequestTest.kt
@@ -0,0 +1,34 @@
+package pozzo.apps.travelweather.core.userinputrequest
+
+import android.arch.lifecycle.LifecycleOwner
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class LocationPermissionRequestTest {
+    @Mock private lateinit var callback: LocationPermissionRequest.Companion.Callback
+    @Mock private lateinit var lifecycleOwner: LifecycleOwner
+
+    private lateinit var locationPermissionRequest: LocationPermissionRequest
+
+    @Before fun setup() {
+        MockitoAnnotations.initMocks(this)
+
+        locationPermissionRequest = LocationPermissionRequest(callback)
+    }
+
+    @Test fun assertCallbackIsBeingCalled() {
+        locationPermissionRequest.granted(lifecycleOwner)
+        locationPermissionRequest.denied()
+
+        verify(callback).granted(lifecycleOwner)
+        verify(callback).denied()
+    }
+
+    @Test fun assertCodeIsNotChanging() {
+        Assert.assertEquals(locationPermissionRequest.code(), locationPermissionRequest.code())
+    }
+}
