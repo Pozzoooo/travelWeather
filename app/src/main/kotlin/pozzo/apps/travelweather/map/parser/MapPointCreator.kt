7165e3e65e4ff7d7fa300c71626e42c6023287c2
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2018-09-09 20:00:29 +0100

    MapPointCreatorTest

--- app/src/main/kotlin/pozzo/apps/travelweather/map/parser/MapPointCreator.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/map/parser/MapPointCreator.kt
@@ -3,6 +3,7 @@ package pozzo.apps.travelweather.map.parser
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.launch
+import pozzo.apps.travelweather.core.CoroutineSettings.background
 import pozzo.apps.travelweather.direction.DirectionWeatherFilter
 import pozzo.apps.travelweather.forecast.ForecastBusiness
 import pozzo.apps.travelweather.forecast.model.point.MapPoint
@@ -14,7 +15,7 @@ class MapPointCreator(
 
     fun createMapPointsAsync(direction: List<LatLng>) : Channel<MapPoint> {
         val mapPoints = Channel<MapPoint>()
-        launch {
+        launch(background) {
             directionWeatherFilter.getWeatherPointsLocations(direction).asSequence()
                     .mapNotNull(forecastBusiness::forecast)
                     .mapNotNull(weatherToMapPointParser::parse)
---
+++ app/src/test/kotlin/pozzo/apps/travelweather/map/parser/MapPointCreatorTest.kt
@@ -0,0 +1,45 @@
+package pozzo.apps.travelweather.map.parser
+
+import com.google.android.gms.maps.model.LatLng
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import pozzo.apps.travelweather.direction.DirectionWeatherFilter
+import pozzo.apps.travelweather.forecast.ForecastBusiness
+import pozzo.apps.travelweather.forecast.model.Weather
+import pozzo.apps.travelweather.forecast.model.point.MapPoint
+
+class MapPointCreatorTest {
+    private lateinit var mapPointCreator : MapPointCreator
+
+    @Mock private lateinit var forecastBusiness: ForecastBusiness
+    @Mock private lateinit var directionWeatherFilter: DirectionWeatherFilter
+    @Mock private lateinit var weatherToMapPointParser: WeatherToMapPointParser
+
+    @Before fun setup() {
+        MockitoAnnotations.initMocks(this)
+        mapPointCreator = MapPointCreator(forecastBusiness, directionWeatherFilter, weatherToMapPointParser)
+    }
+
+    @Test fun assertChannelIsBeingFilled() {
+        val points = listOf(LatLng(1.0, 2.0), LatLng(2.0, 3.0), LatLng(3.0, 4.0))
+        whenever(directionWeatherFilter.getWeatherPointsLocations(points)).thenReturn(points)
+        whenever(forecastBusiness.forecast(any())).thenReturn(Weather("url"))
+        whenever(weatherToMapPointParser.parse(any())).thenReturn(Mockito.mock(MapPoint::class.java))
+
+        val channel = mapPointCreator.createMapPointsAsync(points)
+        var count = 0
+        runBlocking {
+            for (it in channel) {
+                ++count
+            }
+        }
+        Assert.assertEquals(points.size, count)
+    }
+}
