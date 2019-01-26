c98d22deb348b8cb0ab97eca6337312c60006a73
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2019-01-26 20:00:07 +0000

    A couple more scenarios for our weather client tests

---
+++ app/src/test/kotlin/pozzo/apps/travelweather/forecast/WeatherClientTest.kt
@@ -0,0 +1,121 @@
+package pozzo.apps.travelweather.forecast
+
+import com.google.android.gms.maps.model.LatLng
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import pozzo.apps.travelweather.core.FileLoader
+import pozzo.apps.travelweather.core.TestInjector
+import pozzo.apps.travelweather.core.bugtracker.Bug
+import pozzo.apps.travelweather.core.injection.AppComponent
+import pozzo.apps.travelweather.forecast.darksky.DarkSkyClient
+import pozzo.apps.travelweather.forecast.darksky.ForecastModuleDarkSky
+import pozzo.apps.travelweather.forecast.model.Day
+import pozzo.apps.travelweather.forecast.weatherunlocked.ForecastModuleWeatherUnlocked
+import pozzo.apps.travelweather.forecast.weatherunlocked.WeatherUnlockedClient
+import java.lang.Exception
+import java.util.*
+
+//todo add tests for over the limit requests
+class WeatherClientTest {
+    companion object {
+        private const val LATITUDE = -23.565939
+        private const val LONGITUDE = -46.573784
+        private val LAT_LNG = LatLng(LATITUDE, LONGITUDE)
+    }
+
+    private val forecastClients = ArrayList<ForecastClient>()
+
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var appComponent: AppComponent
+    private lateinit var bug: Bug
+
+    @Before fun setup() {
+        appComponent = TestInjector.getAppComponent().build()
+        bug = mock()
+        Bug.setInstance(bug)
+        setupMockServer()
+    }
+
+    private fun setupMockServer() {
+        this.mockWebServer = MockWebServer()
+    }
+
+    @After fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test fun assertHappyPathRequest() {
+        setupAllClients()
+
+        forecastClients.forEach {
+            val weather = it.fromCoordinates(LAT_LNG)!!
+
+            Assert.assertTrue(weather.forecasts.isNotEmpty())
+            Assert.assertEquals(LAT_LNG, weather.address.latLng)
+            Assert.assertNotNull(weather.url)
+            Assert.assertNotNull(weather.poweredBy)
+
+            val forecast = weather.getForecast(Day.TODAY)
+            Assert.assertEquals(ForecastType.SNOW, forecast.forecastType)
+        }
+    }
+
+    private fun setupAllClients(shouldMockResponse: Boolean = true) {
+        setupWeatherUnlocked(if (shouldMockResponse) "weatherUnlockedSample.json" else null)
+        setupDarkSky(if (shouldMockResponse) "darkSkySample.json" else null)
+    }
+
+    private fun setupWeatherUnlocked(mockedResponseJsonFile: String?) {
+        val module = ForecastModuleWeatherUnlocked()
+        val api = module.createApi(appComponent.retrofitBuilder(), enqueueRequest(mockedResponseJsonFile))
+
+        forecastClients.add(WeatherUnlockedClient(api, "", "", module.forecastTypeMapper()))
+    }
+
+    private fun enqueueRequest(jsonFile: String?) : String {
+        if (jsonFile != null) {
+            mockWebServer.enqueue(MockResponse().setBody(FileLoader(jsonFile).string()))
+        } else {
+            mockWebServer.enqueue(MockResponse().setResponseCode(500))
+        }
+        return mockWebServer.url("").toString()
+    }
+
+    private fun setupDarkSky(mockedResponseJsonFile: String?) {
+        val module = ForecastModuleDarkSky()
+        val api = module.createApi(appComponent.retrofitBuilder(), enqueueRequest(mockedResponseJsonFile))
+
+        forecastClients.add(DarkSkyClient(api, module.forecastTypeMapper()))
+    }
+
+    @Test fun shouldNotCrashOnRequestError() {
+        setupAllClients(false)
+
+        forecastClients.forEach {
+            val weather = it.fromCoordinates(LAT_LNG)
+
+            Assert.assertNull(weather)
+        }
+        verify(bug, times(forecastClients.size)).logException(any<Exception>())
+    }
+
+    @Test fun shouldNotCrashAndReportOnErrorFormat() {
+        setupAllClients(false)
+
+        forecastClients.reverse()
+        forecastClients.forEach {
+            val weather = it.fromCoordinates(LAT_LNG)
+
+            Assert.assertNull(weather)
+        }
+        verify(bug, times(forecastClients.size)).logException(any<Exception>())
+    }
+}
