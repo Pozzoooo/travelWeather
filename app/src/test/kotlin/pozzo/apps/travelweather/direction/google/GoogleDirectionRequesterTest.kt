45978c39cb70ac556873629f2f07187f4dcecd7f
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2019-01-26 20:24:08 +0000

    Raising GoogleDirectionRequester test coverage

--- app/src/test/kotlin/pozzo/apps/travelweather/direction/google/GoogleDirectionRequesterTest.kt
+++ app/src/test/kotlin/pozzo/apps/travelweather/direction/google/GoogleDirectionRequesterTest.kt
@@ -1,17 +1,17 @@
 package pozzo.apps.travelweather.direction.google
 
 import com.google.android.gms.maps.model.LatLng
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
 import okhttp3.Call
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.junit.Assert.*
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentCaptor
+import java.io.IOException
 
 class GoogleDirectionRequesterTest {
     private lateinit var requester: GoogleDirectionRequester
@@ -34,5 +34,18 @@ class GoogleDirectionRequesterTest {
         assertNull(response)
         assertTrue(requestUrl.contains("origin=0.0,0.0"))
         assertTrue(requestUrl.contains("&destination=1.0,1.0"))
+    }
+
+    @Test fun shouldNotCrashOnExceptions() {
+        val call = mock<Call> { on { execute() } doThrow RuntimeException("No Whatever! Fake!") }
+        whenever(okHttp.newCall(any())).thenReturn(call)
+        val response = requester.request(LatLng(0.0, 0.0), LatLng(1.0, 1.0))
+        assertNull(response)
+    }
+
+    @Test(expected = IOException::class) fun shouldReThrowOnNetworkIssue() {
+        val call = mock<Call> { on { execute() } doThrow IOException("No Network! Fake!") }
+        whenever(okHttp.newCall(any())).thenReturn(call)
+        val response = requester.request(LatLng(0.0, 0.0), LatLng(1.0, 1.0))
     }
 }
