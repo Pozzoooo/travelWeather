05432ca0ddf34ba38975f91e9d7120e72ab70094
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2019-02-23 22:30:22 +0000

    fixed provider randomization

--- app/src/main/kotlin/pozzo/apps/travelweather/forecast/ForecastModuleAll.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/forecast/ForecastModuleAll.kt
@@ -9,10 +9,10 @@ import kotlin.random.Random
 class ForecastModuleAll : ForecastModule() {
 
     override fun forecastClients(retrofitBuilder: Retrofit.Builder) : List<@JvmSuppressWildcards ForecastClient> {
-        val forecasts = TreeMap<Int, ForecastClient>(kotlin.Comparator { key1, key2 -> key1 - key2 })
+        val forecasts = TreeMap<Int, ForecastClient>(kotlin.Comparator { key1, key2 -> key2 - key1 })
 
-        forecasts[Random.nextInt() % 1000] = ForecastModuleDarkSky().forecastClient(retrofitBuilder)
-        forecasts[Random.nextInt() % 2000] = ForecastModuleWeatherUnlocked().forecastClient(retrofitBuilder)
+        forecasts[Random.nextInt().and(Integer.MAX_VALUE) % 1000] = ForecastModuleDarkSky().forecastClient(retrofitBuilder)
+        forecasts[Random.nextInt().and(Integer.MAX_VALUE) % 2000] = ForecastModuleWeatherUnlocked().forecastClient(retrofitBuilder)
 
         return forecasts.values.toList()
     }
