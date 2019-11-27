a02bcd103079597cf5c42bd37725b867ede2ba7b
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2019-11-27 18:41:17 +0000

    re prioritizing preferences

--- app/src/main/kotlin/pozzo/apps/travelweather/forecast/ForecastModuleAll.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/forecast/ForecastModuleAll.kt
@@ -13,7 +13,7 @@ class ForecastModuleAll : ForecastModule() {
         val forecasts = TreeMap<Int, ForecastClient>(kotlin.Comparator { key1, key2 -> key2 - key1 })
 
         forecasts[Random.nextInt().and(Integer.MAX_VALUE) % 1000] = ForecastModuleDarkSky().forecastClient(retrofitBuilder)
-        forecasts[Random.nextInt().and(Integer.MAX_VALUE) % 2000] = ForecastModuleWeatherUnlocked().forecastClient(retrofitBuilder)
+        forecasts[Random.nextInt().and(Integer.MAX_VALUE) % 20000] = ForecastModuleWeatherUnlocked().forecastClient(retrofitBuilder)
         forecasts[Random.nextInt().and(Integer.MAX_VALUE) % 200] = ForecastModuleOpenWeather().forecastClient(retrofitBuilder)
 
         return forecasts.values.toList()
