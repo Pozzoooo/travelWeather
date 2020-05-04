99db9193b5a58d3825bdc308aa4a40f84b667bce
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2020-05-04 13:32:51 +0100

    ClearActionRequestTest

---
+++ app/src/test/kotlin/pozzo/apps/travelweather/core/action/ClearActionRequestTest.kt
@@ -0,0 +1,18 @@
+package pozzo.apps.travelweather.core.action
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
+import pozzo.apps.travelweather.map.viewmodel.MapViewModel
+
+class ClearActionRequestTest {
+
+    @Test fun assertBound() {
+        val mapViewModel = mock<MapViewModel>()
+
+        ClearActionRequest(mapViewModel).execute()
+
+        verify(mapViewModel).clearStartPosition()
+        verify(mapViewModel).clearFinishPosition()
+    }
+}
