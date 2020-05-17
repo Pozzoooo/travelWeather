8dcd5180b5e7ef8cb9de2879016c1f19a12cd258
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2020-05-17 12:19:59 +0100

    OpenWeatherClient date

--- app/src/main/kotlin/pozzo/apps/travelweather/forecast/openweather/OpenWeatherClient.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/forecast/openweather/OpenWeatherClient.kt
@@ -39,9 +39,12 @@ class OpenWeatherClient(private val api: OpenWeatherApi,
         return dailyData.map { it.asJsonObject }.map {
             val main = it.getAsJsonObject("main")
             val weather = it.getAsJsonArray("weather").get(0).asJsonObject
+            val date = GregorianCalendar.getInstance(TimeZone.getTimeZone("UTC"))
+            date.timeInMillis = it.getAsJsonPrimitive("dt").asLong * 1000L
 
             Forecast(text = weather.get("description").asString,
                     forecastType = forecastTypeMapper.getForecastType(weather.get("main").asString),
+                    date = date,
                     high = main.get("temp_max").asDouble,
                     low = main.get("temp_min").asDouble)
         }
