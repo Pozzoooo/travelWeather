fc11ce8ffcfa059ad9792c85300ce1d516110465
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2020-05-30 10:34:36 +0100

    Better default time

--- app/src/main/kotlin/pozzo/apps/travelweather/forecast/model/Time.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/forecast/model/Time.kt
@@ -5,7 +5,10 @@ import java.util.*
 data class Time(val hour: Int) {
 
     companion object {
-        fun getDefault() = Time(GregorianCalendar.getInstance().get(Calendar.HOUR_OF_DAY))
+        fun getDefault() =
+                Time(GregorianCalendar.getInstance().apply {
+                    roll(Calendar.HOUR_OF_DAY, 1)
+                }.get(Calendar.HOUR_OF_DAY))
     }
 
     override fun toString(): String {
