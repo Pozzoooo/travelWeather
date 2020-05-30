9b8e5448a71226d4a183fea1b433d70bc6c72ac9
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2020-05-30 17:22:26 +0100

    Better temperature message

--- app/src/main/kotlin/pozzo/apps/travelweather/forecast/ForecastTitleFormatter.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/forecast/ForecastTitleFormatter.kt
@@ -11,16 +11,32 @@ class ForecastTitleFormatter @Inject constructor() {
 
     fun createTitle(context: Context, forecast: Forecast): String {
         val forecastString = context.getString(forecast.forecastType.stringId)
+        val tempDiff = forecast.high - forecast.low
+        val temperatureString = if (tempDiff > 7) {
+            parseAsMaxMin(context, forecast)
+        } else {
+            parseAsSingleTemp(forecast)
+        }
+
+        return "$forecastString - $temperatureString"
+    }
+
+    private fun parseAsMaxMin(context: Context, forecast: Forecast): String {
         val min = context.getString(R.string.min)
         val max = context.getString(R.string.max)
         val low = parseTemperature(forecast.low)
         val high = parseTemperature(forecast.high)
-        return "$forecastString - $min: $low $max: $high"
+        return "$min: $low $max: $high"
     }
 
     private fun parseTemperature(fahrenheitTemperature: Double): String {
         val temperature = Temperature()
         temperature.setFahrenheit(fahrenheitTemperature)
         return FormattedTemperature(temperature).getFormattedTemperature()
+    }
+
+    private fun parseAsSingleTemp(forecast: Forecast): String {
+        val averageTemperature = (forecast.low + forecast.high) / 2
+        return parseTemperature(averageTemperature)
     }
 }
