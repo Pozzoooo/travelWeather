2a45dc9681ef9b6d2212743d26b94cea211b2acc
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2020-06-02 05:55:38 +0100

    coroutine test dependency + added test for multi thread error

--- app/src/test/kotlin/pozzo/apps/travelweather/map/parser/WeatherPointsAdapterTest.kt
+++ app/src/test/kotlin/pozzo/apps/travelweather/map/parser/WeatherPointsAdapterTest.kt
@@ -1,18 +1,17 @@
 package pozzo.apps.travelweather.map.parser
 
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.maps.model.LatLng
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
-import pozzo.apps.travelweather.forecast.model.Day
-import pozzo.apps.travelweather.forecast.model.DayTime
-import pozzo.apps.travelweather.forecast.model.Route
-import pozzo.apps.travelweather.forecast.model.Time
+import pozzo.apps.travelweather.forecast.model.*
 import pozzo.apps.travelweather.forecast.model.point.WeatherPoint
+import pozzo.apps.travelweather.map.model.Address
 
-//TODO write these tests
 class WeatherPointsAdapterTest {
     private val weatherPointsData = mock<MutableLiveData<Channel<WeatherPoint>>>()
     private val weatherPointsAdapter = WeatherPointsAdapter(weatherPointsData)
@@ -24,5 +23,27 @@ class WeatherPointsAdapterTest {
         weatherPointsAdapter.updateWeatherPoints(DayTime(Day.TOMORROW, Time.getDefault()), route)
 
         verify(weatherPointsData).postValue(any())
+    }
+
+    @Test fun assertMultipleRequests() {
+        val weatherPoints = Channel<WeatherPoint>()
+        val weatherPoints2 = Channel<WeatherPoint>()
+        val route = Route(weatherPoints = weatherPoints)
+        val route2 = Route(weatherPoints = weatherPoints2)
+        val weatherPoint = WeatherPoint(Weather("", emptyList(), Address(LatLng(.0, .0), ""), PoweredBy(0)))
+
+        //TODO Ta com cara que vou ter que refatorar minhas coroutines
+//        val scope = TestCoroutineScope()
+        weatherPointsAdapter.updateWeatherPoints(DayTime(Day.TOMORROW, Time.getDefault()), route)
+        weatherPointsAdapter.updateWeatherPoints(DayTime(Day.TOMORROW, Time.getDefault()), route2)
+
+
+//        scope.runBlockingTest {
+        runBlocking {
+            weatherPoints.send(weatherPoint)
+            weatherPoints.close()
+            weatherPoints2.send(weatherPoint)
+            weatherPoints2.close()
+        }
     }
 }
