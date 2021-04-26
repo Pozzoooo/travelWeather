ddccd38a167af66634665c0c49ad72c8dc1d4b99
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2021-04-26 14:55:36 -0300

    Removing unnecessary method

--- app/src/main/kotlin/pozzo/apps/travelweather/forecast/weatherunlocked/ForecastModuleWeatherUnlocked.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/forecast/weatherunlocked/ForecastModuleWeatherUnlocked.kt
@@ -2,19 +2,15 @@ package pozzo.apps.travelweather.forecast.weatherunlocked
 
 import pozzo.apps.travelweather.BuildConfig
 import pozzo.apps.travelweather.forecast.ForecastClient
-import pozzo.apps.travelweather.forecast.ForecastTypeMapper
 import retrofit2.Retrofit
 
-//todo do I really need to expose the mapper and the create api? Is there an alternative solution?
 class ForecastModuleWeatherUnlocked {
 
     fun forecastClient(retrofitBuilder: Retrofit.Builder): ForecastClient =
             WeatherUnlockedClient(createApi(retrofitBuilder, baseUrl()), "9e2ec5bf",
-                    BuildConfig.WEATHER_UNLOCKED, forecastTypeMapper())
+                    BuildConfig.WEATHER_UNLOCKED, ForecastTypeMapperWeatherUnlocked())
 
-    fun forecastTypeMapper(): ForecastTypeMapper = ForecastTypeMapperWeatherUnlocked()
-
-    fun createApi(retrofitBuilder: Retrofit.Builder, baseUrl: String): WeatherUnlockedApi {
+    private fun createApi(retrofitBuilder: Retrofit.Builder, baseUrl: String): WeatherUnlockedApi {
         return retrofitBuilder
                 .baseUrl(baseUrl)
                 .build()
