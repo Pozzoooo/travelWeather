6e8d59da67b4eba5ae9457f14f79341a68020681
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2021-07-01 12:33:08 -0300

    Starting multiple waypoints functionality

--- app/src/main/kotlin/pozzo/apps/travelweather/direction/google/GoogleDirectionRequester.kt
+++ app/src/main/kotlin/pozzo/apps/travelweather/direction/google/GoogleDirectionRequester.kt
@@ -8,11 +8,30 @@ import pozzo.apps.travelweather.core.bugtracker.Bug
 import java.io.IOException
 import java.net.URL
 
+/**
+ * https://developers.google.com/maps/documentation/directions/get-directions
+ *
+ * Requests using more than 10 waypoints (between 11 and 25), or waypoint optimization,
+ *          are billed at a higher rate
+ *
+ * TODO URL limit of 8192 characters
+ */
 class GoogleDirectionRequester(private val okHttp: OkHttpClient) {
 
+    companion object {
+        private const val WAYPOINTS_SEPARATOR = "|"
+        private const val COORDINATE_SEPARATOR = ","
+    }
+
     fun request(start: LatLng, end: LatLng): String? {
+        return request(listOf(start, end))
+    }
+
+    private fun request(waypoints: List<LatLng>): String? {
+        if (waypoints.size < 2) return null
+
         return try {
-            val url = createUrl(start, end)
+            val url = createUrl(waypoints)
             val request = Request.Builder().url(url).build()
             okHttp.newCall(request).execute().body()?.string()
         } catch (e: IOException) {
@@ -23,11 +42,36 @@ class GoogleDirectionRequester(private val okHttp: OkHttpClient) {
         }
     }
 
-    private fun createUrl(start: LatLng, end: LatLng) : URL {
-        return URL("https://maps.googleapis.com/maps/api/directions/json?"
-             + "origin=" + start.latitude + "," + start.longitude
-             + "&destination=" + end.latitude + "," + end.longitude
-             + "&sensor=false&units=metric&mode=driving"
-             + "&key=" + BuildConfig.DIRECTIONS)
+    //TODO departure_time
+    private fun createUrl(waypoints: List<LatLng>) : URL {
+        val origin = "origin=" + waypoints.first().latitude +
+                COORDINATE_SEPARATOR + waypoints.first().longitude
+        val destination = "destination=" + waypoints.last().latitude +
+                COORDINATE_SEPARATOR + waypoints.last().longitude
+
+        var urlString = "https://maps.googleapis.com/maps/api/directions/json?" +
+                origin + "&" + destination +
+                "&sensor=false&units=metric&mode=driving" +
+                "&key=" + BuildConfig.DIRECTIONS
+
+        createIntermediateWayPoints(waypoints)?.let {
+            urlString += "&$it"
+        }
+
+        return URL(urlString)
+    }
+
+    private fun createIntermediateWayPoints(waypoints: List<LatLng>): String? {
+        if (waypoints.size <= 2) return null
+
+        val intermediate = waypoints.subList(1, waypoints.lastIndex)//TODO Validar range
+        var intermediateUrlParameter = "waypoints="
+
+        intermediate.forEach {
+            intermediateUrlParameter += "${it.latitude},${it.longitude}$WAYPOINTS_SEPARATOR"
+        }
+
+        val suffixPosition = intermediateUrlParameter.length - WAYPOINTS_SEPARATOR.length
+        return intermediateUrlParameter.substring(0, suffixPosition)
     }
 }
